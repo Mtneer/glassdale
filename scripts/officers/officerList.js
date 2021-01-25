ad898667e640aa06eabeb618523ddb9fcbe74631
David Billups <davidtbillups@gmail.com>
2021-01-25 16:08:59 -0500

    update - end of MOnday

--- scripts/officers/officerList.js
+++ scripts/officers/officerList.js
@@ -9,7 +9,20 @@ export const OfficerList = () => {
         
         let officerHTML = ""
         let officerTarget = document.querySelector(".officer-list")
+
+        // for (let i=0; i<Officers.length; i++) {
+        //     officerHTML += officer(Officers[i])
+        // }
+
+        // for of works with Arrays
+        // for (const variable of Officers) {
+        //     officerHTML += officer(variable)
+        // }
+
         Officers.forEach(officerObject => officerHTML += officer(officerObject))
+
+
+
         officerTarget.innerHTML = officerHTML;
     })
 }
